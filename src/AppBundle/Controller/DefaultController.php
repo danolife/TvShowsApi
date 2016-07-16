c1e1dad2f5f21410f05bfeeb9a901463f2781be5
danolife <badr.dany@gmail.com>
2016-07-16 16:31:21 +0200

    Displays all downloads links of search results

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -20,6 +20,12 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $session = new Session();
+        $token = $session->get('access_token', false);
+        if($token) {
+            return $this->redirectToRoute('shows');
+        }
+
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
         ]);
@@ -31,6 +37,12 @@ class DefaultController extends Controller
      */
     public function loginAction()
     {
+        $session = new Session();
+        $token = $session->get('access_token', false);
+        if($token) {
+            return $this->redirectToRoute('shows');
+        }
+
         $clientId = $this->container->getParameter('client_id');
         $url = "https://www.tvshowtime.com/oauth/authorize";
         $redirectUri = $this->generateUrl('token_request', array(), UrlGeneratorInterface::ABSOLUTE_URL);
@@ -90,17 +102,24 @@ class DefaultController extends Controller
         $response = json_decode($curl->response, true);
 
         if($response['result'] == 'OK') {
-            /*$katPrefix = "https://kat.cr/usearch/";
+            $katPrefix = "https://kat.cr/json.php?q=";
+            $apiReturn = array();
+            $keywords = array();
             foreach ($response['episodes'] as $episode) {
-                $search = strtolower($episode['show']['name'])." S".sprintf("%02d", $episode['season_number'])."E".sprintf("%02d", $episode['number']);
+                $showName = strtolower($episode['show']['name']);
+                $showName = trim(str_replace(array('.', "'"), ' ', $showName));
+                $search = $showName." S".sprintf("%02d", $episode['season_number'])."E".sprintf("%02d", $episode['number']);
                 $curl = new Curl();
-                dump($katPrefix.str_replace(' ', '%20', $search));
-                $curl->get($katPrefix.str_replace(' ', '%20', $search));
-                dump($curl->response);
+                $curl->get($katPrefix.urlencode($search));
+                $keywords[] = $search;
+                $apiReturn[] = json_decode($curl->response, true);
             }
-            die();*/
 
-            return $this->render('default/shows.html.twig', array('episodes' => $response['episodes']));
+            return $this->render('default/shows.html.twig', array(
+                'episodes' => $response['episodes'],
+                'apiReturn' => $apiReturn,
+                'keywords' => $keywords
+            ));
         } else {
             return $this->render('default/apiError.html.twig', array('errorMessage' => $response['message']));
         }
